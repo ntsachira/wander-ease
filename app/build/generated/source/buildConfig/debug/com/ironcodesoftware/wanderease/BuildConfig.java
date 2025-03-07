a4a6e42a36d0e9637711c6fd40ffc92c9659590d
Sachira Jayawardana <sachira.jayawardana@mail.bcu.ac.uk>
2025-03-07 14:21:42 +0530

    initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/ironcodesoftware/wanderease/BuildConfig.java
@@ -0,0 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ironcodesoftware.wanderease;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ironcodesoftware.wanderease";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from build type: debug
+  public static final String HOST_URL = "https://oriented-promptly-bee.ngrok-free.app/WanderEase/";
+  public static final String MAPS_API_KEY = "AIzaSyDKKOSBsL4MzWzVYyi66E_DajIc27IT4j4";
+}
